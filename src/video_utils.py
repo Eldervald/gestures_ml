44871d25363a6b524c04b0150f604a83d618288a
eldervald <nikolay.ignatenko@outlook.com>
2023-06-06 08:25:09 +0000

    Updated conv_transformer transforms

---
+++ src/video_utils.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    
+    timestamps = []
+    cur_timestamp = 0
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames.append(frame)
+        timestamps.append(cur_timestamp)
+        cur_timestamp += int(1.0 / fps * 1e3)
+        
+    frames = np.array(frames)
+    timestamps = np.array(timestamps)
+    
+    return frames, timestamps
